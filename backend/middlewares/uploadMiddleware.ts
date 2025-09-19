a8e2fcc7cd7302865f5645f8b7d259e1fa51aece
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-09-19 12:54:17 +0500

    updateimg

--- backend/middlewares/uploadMiddleware.ts
+++ backend/middlewares/uploadMiddleware.ts
@@ -1,19 +1,18 @@
 import multer from "multer";
-import path from "path";
-import * as fs from 'fs';
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-const uploadDir = "uploads/";
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}    
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Ensure this folder exists
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    public_id: (req: any, file: any) => `products/${file.originalname}`,
+  },
 });
 
 export const upload = multer({ storage });
