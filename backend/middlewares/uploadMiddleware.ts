45f9c385410c1f30b8f001506943f8df50d335a9
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-09-19 12:41:30 +0500

    filefix

--- backend/middlewares/uploadMiddleware.ts
+++ backend/middlewares/uploadMiddleware.ts
@@ -2,19 +2,18 @@ import multer from "multer";
 import path from "path";
 import * as fs from 'fs';
 
-const uploadDir = "uploads/images/";
+const uploadDir = "uploads/";
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }    
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Ensure this folder exists
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
 });
-
 
 export const upload = multer({ storage });
