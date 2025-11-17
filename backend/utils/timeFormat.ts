e61a55d110044f0c61c189e3bd1904eef19a3e0b
awais2000 <owaisansar00x@gmail.com>
2025-11-17 22:32:24 +0500

    update

--- backend/utils/timeFormat.ts
+++ backend/utils/timeFormat.ts
@@ -23,5 +23,5 @@ export function formatDateToDDMMYYYY(dateInput: string | Date): string {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
-  return `${day}:${month}:${year}`;
+  return `${day}-${month}-${year}`;
 }
